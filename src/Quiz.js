ace15f34537d649051bbeac38f214c04e1268639
Jonathan Brincat <jonathan@pix8.co.uk>
2020-08-15 12:42:00 +0100

    syntax error my bad

--- src/Quiz.js
+++ src/Quiz.js
@@ -32,7 +32,7 @@ class Quiz extends Component {
 			<div>
 				{
 					isQuizEnd ?
-						<QuizEnd resetClickHandler="this.handleResetClick.bind(this)" />
+						<QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
 						:
 						<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={ this.showNextQuestion.bind(this) } />
 				}
